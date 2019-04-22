804c383536bb36571a9fd6c32ace8c183917d0fa
wunanlieh <lieh1959@gmail.com>
2019-04-22 12:17:04 +0800

    first

--- Class03/ViewController.swift
+++ Class03/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var lbmessage: UILabel!
+    
+
+    @IBAction func bt01(_ sender: UIButton) {
+        lbmessage.text="hi,happy"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
