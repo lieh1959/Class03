88ebe6a51fbe635267294c12af32bd90bd0ebde6
lieh1959 <lieh1959@gmail.com>
2019-05-13 12:03:49 +0800

    0513-1

--- Class03/MyBrowserViewController.swift
+++ Class03/MyBrowserViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import WebKit
 
-class MyBrowserViewController: UIViewController {
+class MyBrowserViewController: UIViewController,UITextFieldDelegate {
+    @IBOutlet weak var btnGoBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var myWebView: WKWebView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        myWebView.load(URLRequest(url: URL(string:  "https://www.google.com")!))
         // Do any additional setup after loading the view.
     }
     
@@ -36,18 +41,40 @@ class MyBrowserViewController: UIViewController {
         NotificationCenter.default.removeObserver(self, name:
             UIResponder.keyboardWillHideNotification, object: nil)
     }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    //Mark: -TextField
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let accept="abcdeABCDE"
+        let cs=NSCharacterSet(charactersIn: accept).inverted
+        let filters=string.components(separatedBy: cs).joined(separator: "")
+        if(string != filters)
+        {
+            return false
+        }
+        
+        
+        return true
+    }
     
     @objc func keyboardWillAppear(notification:NSNotification?){
         print("keyboarWillAppear")
+
+      guard let frame=notification?.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {
+            return
+        }
+        self.btnGoBottomConstraint.constant=frame.cgRectValue.height;
     }
     
     @objc func keyboardWillDisAppear(notification:NSNotification?){
         print("keyboarWillDisAppear")
+        self.btnGoBottomConstraint.constant=31;
     }
     
     /*
     // MARK: - Navigation
-
+     
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
